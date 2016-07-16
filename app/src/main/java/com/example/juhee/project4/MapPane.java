17035b9bb6f42a05e82c7dfd05147c5bbef693c8
eunji6546 <eunji6546@gmail.com>
2016-07-16 19:33:21 +0900

    GPS

--- app/src/main/java/com/example/juhee/project4/MapPane.java
+++ app/src/main/java/com/example/juhee/project4/MapPane.java
@@ -1,43 +1,121 @@
 package com.example.juhee.project4;
 
+import android.*;
+import android.Manifest;
+import android.app.AlertDialog;
+import android.app.Service;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.IBinder;
+import android.provider.Settings;
+import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentActivity;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.app.Activity;
 import android.util.Log;
 
-import com.google.android.gms.maps.*;
-import com.google.android.gms.maps.model.*;
 
-public class MapPane extends Activity implements OnMapReadyCallback {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.util.TimerTask;
+
+public class MapPane extends FragmentActivity implements OnMapReadyCallback {
+
+    // GPSTracker class
+    private GpsInfo gps;
+    double latitude, longitude;
+    private int MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION=100;
+    GoogleMap _map;
+
+    private TimerTask second;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.map_activity);
 
-        MapFragment mapFragment = (MapFragment) getFragmentManager()
-                .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
-    }
-
-    @Override
-    public void onMapReady(GoogleMap map) {
-        LatLng sydney = new LatLng(-33.867, 151.206);
 
         if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
                 && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            Log.e("MapPane","onMapReady permission reject!!");
+            Log.e("MapPane","********************");
+
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
+
+
             return;
         }
-        map.setMyLocationEnabled(true);
-        map.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 13));
 
-        map.addMarker(new MarkerOptions()
-                .title("Sydney")
-                .snippet("The most populous city in Australia.")
-                .position(sydney));
+
+         gps = new GpsInfo(getApplicationContext());
+
+        SupportMapFragment mapFragment = (SupportMapFragment)this.getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(MapPane.this);
+
     }
+
+        @Override
+        public void onMapReady (GoogleMap map){
+
+            if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                    && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                Log.e("MapPane","13131313131313");
+            } Log.e("Hi","HHHHHHHHHHHHHHHHHHHH");
+            _map = map;
+            if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                Log.e("Hi","iiiiiiiiiiiiiiiiiiii");
+                return;
+            }
+
+            if (gps.isGetLocation()) {
+                Log.e("GG","GGGGGETTTT");
+                latitude = gps.getLatitude();
+                longitude = gps.getLongitude();
+                LatLng now = new LatLng(latitude,longitude);
+                map.setMyLocationEnabled(true);
+                map.getUiSettings().setZoomControlsEnabled(true);
+                map.getUiSettings().setRotateGesturesEnabled(false);
+                map.moveCamera(CameraUpdateFactory.newLatLng(now));
+                map.getUiSettings().setMyLocationButtonEnabled(true);
+                map.animateCamera(CameraUpdateFactory.zoomTo(16));
+                _map.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+
+            } else {
+                // GPS 를 사용할수 없으므로
+                gps.showSettingsAlert();
+            }
+
+            //second =
+        }
+
+     /*       LatLng sydney = new LatLng(-33.867, 151.206);
+
+
+            if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                    && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                Log.e("MapPane","onMapReady permission reject!!");
+                return;
+            }
+            map.setMyLocationEnabled(true);
+            map.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 13));
+
+
+            map.addMarker(new MarkerOptions()
+                    .title("Sydney")
+                    .snippet("The most populous city in Australia.")
+                    .position(sydney));
+        }*/
 }
