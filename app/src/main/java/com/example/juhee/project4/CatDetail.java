e4f4b9beab965b64905045f12fd179b5c3291239
eunji6546 <eunji6546@gmail.com>
2016-07-19 19:53:59 +0900

    more sockets

---
+++ app/src/main/java/com/example/juhee/project4/CatDetail.java
@@ -0,0 +1,13 @@
+package com.example.juhee.project4;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CatDetail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cat_detail);
+    }
+}
---
+++ app/src/main/java/com/example/juhee/project4/CatDetailAdapter.java
@@ -0,0 +1,7 @@
+package com.example.juhee.project4;
+
+/**
+ * Created by q on 2016-07-19.
+ */
+public class CatDetailAdapter {
+}
